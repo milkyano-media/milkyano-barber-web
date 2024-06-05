4fb0c17d13162d36de740096cef29bcb22c8bf6a
aldovadev <aldova811@gmail.com>
2024-06-06 03:48:11 +0700

    add : routes and file structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import AppRoutes from './routes';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Landing/Josh/JoshLanding.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const JoshLanding: React.FC = () => {
+  return (
+    <div>
+      <h1>Josh Landing Page</h1>
+      <p>Welcome to the Josh Landing Page.</p>
+    </div>
+  );
+}
+
+export default JoshLanding;
