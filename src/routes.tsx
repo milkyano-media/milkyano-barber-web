0d3c3b8c1682f87c4adb52f381c11aab6f797146
aldovadev <aldova811@gmail.com>
2024-06-18 14:52:26 +0700

    fix : barber page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
+import Barbers from './pages/Barbers';
 import NotFound from './pages/NotFound';
 import JoshLanding from './pages/Landing/Josh/JoshLanding';
 
@@ -10,8 +10,8 @@ const AppRoutes: React.FC = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/barbers" element={<Barbers />} />
         <Route path="/josh" element={<JoshLanding />} />
-        <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
