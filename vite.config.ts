8bb3fa9a6dbaec37aed857aa86be5a5e59a9daf4
aldovadev <aldova811@gmail.com>
2024-07-02 14:11:12 +0700

    fix: dockerfile and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import path from 'path';
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [react(),
+
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
