2180f4e486e64947255bba0b1989c6349d7d5f86
aldovadev <aldova811@gmail.com>
2024-07-02 15:16:56 +0700

    fix : vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { VitePluginRadar, } from 'vite-plugin-radar'
 
 export default defineConfig({
   plugins: [react(),
-
+  VitePluginRadar({
+    enableDev: true,
+    gtm: [
+      {
+        id: 'GTM-W94TJ64',
+      }
+    ],
+  }),
   ],
+  build: {
+    chunkSizeWarningLimit: 1600
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
