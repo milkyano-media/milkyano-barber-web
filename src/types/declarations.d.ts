1fd39095b65e8e5a010e401f3ade1a15482233d4
aldovadev <aldova811@gmail.com>
2024-07-02 14:51:31 +0700

    fix: declarations

---
+++ src/types/declarations.d.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module '@/pages/landing/JoshLanding' {
+  const content: any;
+  export default content;
+}
+
+declare module '@/pages/landing/WyattLanding' {
+  const content: any;
+  export default content;
+}
+
+declare module '@/pages/landing/RayhanLanding' {
+  const content: any;
+  export default content;
+}
+
+declare module '@/pages/landing/JayLanding' {
+  const content: any;
+  export default content;
+}
+
+declare module '@/pages/landing/NikoLanding' {
+  const content: any;
+  export default content;
+}
+
+declare module '@/pages/landing/EmmanLanding' {
+  const content: any;
+  export default content;
+}
+
+declare module '@/pages/landing/DejanLanding' {
+  const content: any;
+  export default content;
+}
+
+declare module '@/pages/landing/ChristosLanding' {
+  const content: any;
+  export default content;
+}
+
+declare module '@/pages/landing/AnthonyLanding' {
+  const content: any;
+  export default content;
+}
