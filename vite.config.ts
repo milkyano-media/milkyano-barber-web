cf3df6079b8618cfeb055c43cba732b6abf45510
aldovadev <aldova811@gmail.com>
2024-07-02 13:47:14 +0700

    fix : tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { VitePluginRadar, } from 'vite-plugin-radar'
 
 export default defineConfig({
-  plugins: [react(),
-  VitePluginRadar({
-    enableDev: true,
-    gtm: [
-      {
-        id: 'GTM-W94TJ64',
-      }
-    ],
-  }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
