bd8f3f2d486324103004fde45146c4c87e0c406d
aldovadev <aldova811@gmail.com>
2024-07-02 16:36:59 +0700

    fix : deployment #11

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import { VitePluginRadar } from 'vite-plugin-radar';
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { VitePluginRadar, } from 'vite-plugin-radar'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePluginRadar({
-      enableDev: true,
-      gtm: [
-        {
-          id: 'GTM-W94TJ64',
-        }
-      ],
-    }),
+  plugins: [react(),
+  VitePluginRadar({
+    enableDev: true,
+    gtm: [
+      {
+        id: 'GTM-W94TJ64',
+      }
+    ],
+  }),
   ],
   build: {
     chunkSizeWarningLimit: 1600
@@ -23,4 +22,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
