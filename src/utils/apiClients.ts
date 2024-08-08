979462d4dfbcbc62bbb47f4438c962d904794685
aldovadev <aldova811@gmail.com>
2024-08-08 14:49:58 +0700

    fix : api base url

--- src/utils/apiClients.ts
+++ src/utils/apiClients.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiSquare = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL_SQUARE as string,
+  baseURL: import.meta.env.VITE_API_WEB_BASE_URL as string,
   headers: {
     'x-api-key': import.meta.env.VITE_API_KEY_SQUARE as string,
   },
